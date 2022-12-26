3343d00859a95c214e880eb95d0797978a16e09c
pvlustinov <pvlustinov@gmail.com>
2022-12-27 00:16:57 +0100

    uptade the server url

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from sever, bot's reponse
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-its6.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
